20e813923801f605dc309560f43441ca8736d642
devYeonjun <alcls0026@gmail.com>
2022-05-25 20:53:54 +1200

    Add props & typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+
+import "./App.css";
+import Todos from "./compoenents/Todos";
 
 function App() {
-  return (
-    <div>
-   
-    </div>
-  );
+	return (
+		<div>
+			<Todos items={['Learn React', 'Learn Typescript']}/>
+		</div>
+	);
 }
 
 export default App;
